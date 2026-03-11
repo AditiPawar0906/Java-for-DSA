6cc0b45b4b6155304cfbe55e8d8a6ac2915914dd
AditiPawar0906 <abhinowpawar611@gmail.com>
2026-03-11 23:31:29 +0530

    Add files via upload

---
+++ methods1.java
@@ -0,0 +1,13 @@
+public class methods1{
+    public void heloo()
+    {
+        System.out.println("Hello everyone");
+    }
+    public static void main(String args[])
+    {
+        methods1  h1= new methods1();
+        h1.heloo();
+
+
+    }
+}
